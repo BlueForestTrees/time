1b5e582b63d2deb1578167b09f7d49cd452e4eb5
slimee <smedini@gmail.com>
2015-10-15 19:31:14 +0200

    avance filtre phrases

---
+++ Time/time.transformer/src/main/java/time/transformer/component/filter/PhraseFilter.java
@@ -0,0 +1,15 @@
+package time.transformer.component.filter;
+
+import org.springframework.stereotype.Component;
+
+import time.repo.bean.Phrase;
+
+@Component
+public class PhraseFilter {
+
+	public boolean keepThisPhrase(Phrase phrase){
+		boolean startWithCatogories = phrase.getText().startsWith("Catégories :");
+		//boolean tooLong = phrase.getText().length() > 1000;
+		return !startWithCatogories;
+	}
+}
---
+++ Time/time.transformer/src/main/java/time/transformer/config/TransformerConfig.java
@@ -0,0 +1,46 @@
+package time.transformer.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import time.transformer.component.reader.SmartScanner;
+
+@Configuration
+@ComponentScan({"time.transformer.service", "time.transformer.component", "time.transformer.tool"})
+@Import({TransformerDbConfig.class, DateFinderConfig.class})
+public class TransformerConfig {
+	@Bean
+	public String path(){
+		return "c:/time/data";
+	}
+	
+	@Bean
+	public String baseUrl(){
+		return "http://fr.wikipedia.org/wiki/";
+	}
+	
+	@Bean
+	public int urlMaxLength(){
+		return 255;
+	}
+	
+	@Bean
+	public Long pageTotal(){
+		return 2000000L;
+	}
+	
+	@Bean String sep(){
+		return "\\|¨";
+	}
+	
+	@Bean
+	public SmartScanner scanner() throws IOException{
+		SmartScanner scanner = new SmartScanner(path());
+		scanner.setDelimiter(sep());
+		return scanner;
+	}
+}

e0559214720e833959cfc009e7cba3b65b6dacff
slimee <smedini@gmail.com>
2015-11-07 23:47:57 +0100

    go go lucene!

--- Time/time.transformer/src/main/java/time/transformer/service/FindPhrasesModule.java
+++ Time/time.transformer/src/main/java/time/transformer/service/FindPhrasesModule.java
@@ -11,20 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 import time.repo.bean.Page;
 import time.transformer.component.filter.PageFilter;
 import time.transformer.component.reader.FinDuScanException;
-import time.transformer.repo.PageRepository;
-import time.transformer.repo.PhraseRepository;
 import time.transformer.tool.handler.PhraseHandler;
 
 @Service
-public class FindPhrasesModule implements IModule {
+public class FindPhrasesLuceneModule implements IModule {
 
-    private static final Logger log = LogManager.getLogger(FindPhrasesModule.class);
-
-    @Autowired
-    PageRepository pageRepository;
-
-    @Autowired
-    PhraseRepository phraseRepository;
+    private static final Logger log = LogManager.getLogger(FindPhrasesLuceneModule.class);
 
     @Autowired
     PageReaderService pageReader;
@@ -38,29 +30,17 @@ public class FindPhrasesModule implements IModule {
     @Transactional
     public void onStart() {
         log.info("onStart()");
-        pageRepository.deleteAll();
-        phraseRepository.deleteAll();
     }
 
     @Transactional
     public long run(long pageCount) throws IOException, FinDuScanException {
-        System.out.println();
-        System.out.println();
-        System.out.println("NEW DATA PAGE");
-        System.out.println();
-        System.out.println();
         long phraseCount = 0;
         for (long i = 0; i < pageCount; i++) {
             Page page = pageReader.getNextPage();
             if (pageFilter.isValidPage(page)) {
                 if (pageFilter.isNewPage(page)) {
-                    page = pageRepository.save(page);
-                } else {
-                    page.setId(pageRepository.getIdByUrl(page.getUrl()));
+                    phraseCount += phraseHandler.handle(page);
                 }
-
-                phraseCount += phraseHandler.handle(page);
-
                 pageFilter.rememberThisPage(page);
             }
         }

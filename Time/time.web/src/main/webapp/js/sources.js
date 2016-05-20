a6dd2e0da4abe1a3645d1c770678262827e3580a
Slimane <SMedini.prestataire@voyages-sncf.com>
2016-05-20 17:34:02 +0200

    front in progress

--- Time/time.web/src/main/webapp/js/sources.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    Time.sources = {
-            "sapiens" : {
-                title : "Sapiens, Une brève histoire de l'Humanité - Yuval Noah Harari",
-                url : "http://www.albin-michel.fr/Sapiens-EAN=9782226257017"
-            }
-    };
-})();
--- Time/time.web/webpack.config.js
+++ Time/time.web/webpack.config.js
@@ -1,54 +1,28 @@
 var webpack = require('webpack');
-var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ENV = process.env.npm_lifecycle_event;
-var isProd = ENV === 'build';
+var isProd = false;//ENV === 'build';
+var srcDir = __dirname + "/src/main/html.js";
 
-module.exports = function makeWebpackConfig () {
-	var config = {};
-	config.debug = !isProd;
-	config.entry = { index:  ['./src/main/webapp/app.js'] };
-	config.output = {
+module.exports = {
+	debug: !isProd,
+    entry: srcDir + "/app.js",
+    output: {
 		path : __dirname + '/target/webapp',
 		publicPath : '/',
 		filename : 'js/[name].[hash].js',
 		chunkFilename : '[id].[hash].chunk.js'
-	};
-	config.resolve = {
-		root : __dirname + '/src/main/webapp',
-		extensions : ['', '.js', '.json', '.css', '.html']
-	};
-	config.module = {
-		preLoaders : [],
-		loaders : [
-			{ test : /\.js$/,   exclude: /node_modules/, loaders: [] },
-			{ test : /\.json$/, loader : 'json' },
-			{ test : /\.css$/,  loaders : ['style','css'] },
-			{ test : /\.html$/, loader : 'raw'}
-		],
-		postLoaders : [],
-		noParse : []
-	};
-	config.plugins = [
-		new webpack.DefinePlugin({ 'process.env' : { ENV : JSON.stringify(ENV) } }),
-		new HtmlWebpackPlugin({
-			template : './src/main/webapp/view/histoires.html',
-			inject : 'body',
-			hash : 'true'
-		})
-	];
-	if (isProd) {
-		config.plugins.push(
-			new webpack.optimize.UglifyJsPlugin(),
-			new CopyWebpackPlugin([{from : __dirname + '/src/main/webapp/public'}])
-		);
-	}
-	config.postcss = [autoprefixer({ browsers : ['last 2 version']})];
-	config.devServer = {
-		contentBase : './src/public',
-		historyApiFallback : false,
-		stats : 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
-	};
-	return config;
-}();
+	},
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    },commonschunkplugin
+	plugins: [
+		new HtmlWebpackPlugin({template : srcDir + '/index.html',inject : 'body',hash : 'true'})
+	]
+};
+
+if(!isProd){
+    module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}

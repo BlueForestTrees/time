259fc453eea668531a7487fc3216b92b8ca01395
Slimane <SMedini.prestataire@voyages-sncf.com>
2016-06-21 17:45:34 +0200

    scripts

--- Time/time.meta.to.index/src/main/java/time/meta/to/index/MetaToIndexMain.java
+++ Time/time.meta.to.index/src/main/java/time/meta/to/index/MetaToIndexMain.java
@@ -14,22 +14,23 @@ public class MetaToIndexMain {
 
     private static final Logger LOGGER = LogManager.getLogger(MetaToIndexMain.class);
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
 
-        final Messager messager = new Messager();
-
-        final MetaToIndexService metaToIndexService = Guice.createInjector(new MetaToIndexModule()).getInstance(MetaToIndexService.class);
-
-        messager.when(Queue.META_CREATED, Meta.class)
-                .then(meta -> {
-                    try {
-                        final IndexCreation indexCreation = metaToIndexService.run(meta);
-                        messager.signal(Queue.INDEX_CREATED, indexCreation);
-                    } catch (InvocationTargetException | IllegalAccessException e) {
-                        LOGGER.error(e);
-                    }
-                });
-
+        try {
+            final Messager messager = new Messager();
+            final MetaToIndexService metaToIndexService = Guice.createInjector(new MetaToIndexModule()).getInstance(MetaToIndexService.class);
+            messager.when(Queue.META_CREATED, Meta.class)
+                    .then(meta -> {
+                        try {
+                            final IndexCreation indexCreation = metaToIndexService.run(meta);
+                            messager.signal(Queue.INDEX_CREATED, indexCreation);
+                        } catch (InvocationTargetException | IllegalAccessException e) {
+                            LOGGER.error(e);
+                        }
+                    });
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
     }
 
 }
